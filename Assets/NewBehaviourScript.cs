65a27718881bd411e7995e3b7fa53708b3f6b595
stwalsh4118 <40175938+stwalsh4118@users.noreply.github.com>
2021-04-01 16:45:00 -0500

    MRTK RubiksCube

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(4.2f, 2.5f, 11f);
+        transform.localEulerAngles = new Vector3(-31f, -27f, 33f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
